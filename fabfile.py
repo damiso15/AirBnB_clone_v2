75b099cd39f589203ac0e14cc8511a19c3f80d6c
damiso15 <damiso15@yahoo.com>
2023-07-18 13:56:24 +0100

    1. Compress before sending

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of a directory
+"""
+
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+    local("mkdir -p versions")
+    result = local("tar -czvf {} web_static".format(archive_path))
+    if result.failed:
+        return None
+    return archive_path
+
